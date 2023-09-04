938486c3e40aedbed0ad0f21247af41015088d14
Shankar Chakraborty <40225075+Shankar022@users.noreply.github.com>
2023-09-04 20:51:16 +0530

    Returning objects from controller

--- src/main/java/com/shankar/com/courseapi/topic/TopicController.java
+++ src/main/java/com/shankar/com/courseapi/topic/TopicController.java
@@ -1,4 +1,20 @@
 package com.shankar.com.courseapi.topic;
+ 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
 public class TopicController {
+
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics() {
+        return Arrays.asList(
+                new Topic("Java", "Java Programming", "Java Programming description"),
+                new Topic("Spring", "Spring Boot", "Spring Boot description"),
+                new Topic("Javascript", "Javascript Programming", "Javascript Programming description")
+                );
+    }
 }
